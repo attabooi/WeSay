832c12586045bc2a477e356ff3a5c8bb37309b84
Joon <joonqpa@gamil.com>
2025-01-11 23:26:37 +0900

    modify app.py

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ document.getElementById("translate-btn").addEventListener("click", async () => {
   }
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/translate", {
+    const response = await fetch("https://wesay.onrender.com/translate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
